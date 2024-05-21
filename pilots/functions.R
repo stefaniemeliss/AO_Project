94c61bebda86623c96c2133165cff7d277f453d1
Stefanie Meliss, PhD <s_meliss@outlook.com>
2024-05-21 10:48:49 +0100

    update figure theme

--- pilots/functions.R
+++ pilots/functions.R
@@ -8,22 +8,22 @@ library(extrafont)
 ambition_coral <- "#E94B58"
 ambition_charcole <- "#2C3C3B"
 
-# ambition colours 2023 #
-navy = "#474C68"
-white = "#FFFFFF"
-black = "#000000"
-cyan = "#14B4E9"
-coral = "#E94B58"
-yellow = "#FFCC00"
-green = "#C1D10F"
-teal = "#00987C"
-blue = "#006FB7"
-purple = "#6D2160"
-orange = "#EC642D"
-red = "#BF1C1D"
+# ambition colours
+navy    = "#474C68"
+cyan    = "#14B4E9"
+coral   = "#E94B58"
+teal    = "#00987C"
+purple  = "#6D2160"
+orange  = "#EC642D"
+yellow  = "#FFCC00"
+blue    = "#006FB7"
+red     = "#BF1C1D"
+white   = "#FFFFFF"
+black   = "#000000"
 
+# function to create colour tints
 # source: https://rdrr.io/cran/MESS/src/R/colorfunctions.R
-col.tint <- function(col, tint=.5) {
+col.tint <- function(col, tint=.4) {
   
   if(missing(col))
     stop("a vector of colours is missing")
@@ -35,14 +35,49 @@ col.tint <- function(col, tint=.5) {
   rgb(mat, alpha=mat[,4], maxColorValue=255)
 }
 
+# generate tints #
+
+# navy
+navy40 <- col.tint(navy, tint = .4)
+navy30 <- col.tint(navy, tint = .3)
+navy20 <- col.tint(navy, tint = .2)
+navy10 <- col.tint(navy, tint = .1)
+palette_navy <- c(navy, navy40, navy30, navy20, navy10)
+
+# black
+black40 <- col.tint(black, tint = .4)
+black30 <- col.tint(black, tint = .3)
+black20 <- col.tint(black, tint = .2)
+black10 <- col.tint(black, tint = .1)
+palette_black <- c(black, black40, black30, black20, black10)
+
+# load libraries
+library(ggplot2)
+library(extrafont)
+# if not run before, run
+# font_import()
+
+# define font sizesfor normal text and headings in pt
+font_size = 12
+head_size = font_size + 2
+
 # define theme for plot
-theme <- theme_bw() + theme(
-  text=element_text(family="Segoe UI"),
-  plot.title = element_text(size=14, face="bold"),
-  axis.title = element_text(size=10, face="bold"),
-  legend.title = element_blank(),
-  legend.position = "bottom"
-)
+ambition_theme <- theme_bw(base_family = "Segoe UI") + 
+  theme(
+    text = element_text(family = "Segoe UI", size = font_size),
+    title = element_text(face = "bold", size = font_size),
+    plot.title = element_text(size = head_size, face = "bold"),
+    plot.subtitle = element_text(size = font_size, face = "plain"),
+    axis.title = element_text(size = font_size, face = "bold"),
+    axis.text = element_text(size = font_size),
+    legend.title = element_text(size = font_size, face = "bold"),
+    legend.text = element_text(size = font_size),
+    strip.text = element_text(size = font_size),
+    plot.caption = element_text(size = font_size),
+    strip.background = element_rect(fill = black20),
+    legend.position = "bottom"
+  )
+
 
 dominant_col <- coral
 nondominant_col <- navy
